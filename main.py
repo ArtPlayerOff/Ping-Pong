e7e7e159713d0d559026241466e37a505ed3806d
ArtPlayerOff <134778235+ArtPlayerOff@users.noreply.github.com>
2023-05-31 16:52:54 +0300

    Добавили отскакивания и надпись проигрыша

--- main.py
+++ main.py
@@ -46,6 +46,11 @@ racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tennis_ball.png', 200, 200, 4, 50, 50)
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('ИГРОК 1 ПРОИГРАЛ!', True, (180, 0, 0))
+lose2 = font.render('ИГРОК 2 ПРОИГРАЛ!', True, (180, 0, 0))
+
 speed_x = 3
 speed_y = 3
 
@@ -60,6 +65,21 @@ while game:
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+        
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
 
         racket1.reset()
         racket2.reset()
